4bec1e35c1760a27c0f22bd6d62741e3913e6158
Diegoo11 <ynoacamino@gmail.com>
2024-02-14 11:17:04 -0500

    Add TagsPage component

---
+++ src/app/tags/page.tsx
@@ -0,0 +1,11 @@
+import H1 from '@/components/ui/H1';
+
+export default function TagsPage() {
+  return (
+    <div className="p-10">
+      <H1>
+        Ya me dio flojera
+      </H1>
+    </div>
+  );
+}
